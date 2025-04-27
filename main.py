0e50370df42c9ec33b26aad0b1981b4618739b31
guilhermerb7 <gbdevelopment0@gmail.com>
2025-04-26 21:11:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import mysql.connector
 conn = mysql.connector.connect(
     host='localhost',
     user='root',
-    password='guizin123',
+    password='SuaSenhaAqui',
     database='escola'
 )
 cursor = conn.cursor()
